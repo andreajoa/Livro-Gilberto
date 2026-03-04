a473395044c7f6339a9177c0bb5e044c34dfc1d2
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-03-04 17:35:05 -0300

    feat: fade transition between pages + ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -13,24 +13,63 @@ import Terms from './pages/Termos';
 import Delivery from './pages/Entrega';
 import Return from './pages/Devolucao';
 
+const fadeVariants = {
+  initial: { opacity: 0, y: 20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.45, ease: [0.25, 0.46, 0.45, 0.94] }
+  },
+  exit: {
+    opacity: 0,
+    y: -10,
+    transition: { duration: 0.25 }
+  }
+};
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  const { useEffect } = require('react');
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]);
+  return null;
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        variants={fadeVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+          <Route path="/o-livro" element={<Book />} />
+          <Route path="/contato" element={<Contact />} />
+          <Route path="/privacidade" element={<Privacy />} />
+          <Route path="/termos" element={<Terms />} />
+          <Route path="/entrega" element={<Delivery />} />
+          <Route path="/devolucao" element={<Return />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <HelmetProvider>
       <div className="min-h-screen flex flex-col">
         <Navbar />
+        <ScrollToTop />
         <main className="flex-grow">
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<About />} />
-              <Route path="/o-livro" element={<Book />} />
-              <Route path="/contato" element={<Contact />} />
-              <Route path="/privacidade" element={<Privacy />} />
-              <Route path="/termos" element={<Terms />} />
-              <Route path="/entrega" element={<Delivery />} />
-              <Route path="/devolucao" element={<Return />} />
-            </Routes>
-          </AnimatePresence>
+          <AnimatedRoutes />
         </main>
         <Footer />
         <WhatsAppButton />
